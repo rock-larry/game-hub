7e837462b5b4f58fa397b21380a7aa8a659b3178
rock-lee <rockleegates92@gmail.com>
2025-08-28 00:35:46 +0000

    rewrote the css for index.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("header.html")
+  fetch("html/header.html")
     .then(res => res.text())
     .then(data => document.getElementById("header").innerHTML = data);
 
-  fetch("footer.html")
+  fetch("html/footer.html")
     .then(res => res.text())
     .then(data => document.getElementById("footer").innerHTML = data);
 });
