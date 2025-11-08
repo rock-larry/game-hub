54f269bfe1e2fd8c8a93bacf1aed9bed1b19b1ab
rock-lee <rockleegates92@gmail.com>
2025-11-08 00:51:11 +0000

    nn

--- server/server.py
+++ server/server.py
@@ -13,7 +13,7 @@ ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 # ----------------------------
 # Path to the web folder
 # ----------------------------
-WEB_FOLDER = os.path.join(os.path.dirname(__file__), "..", "game-hub")
+WEB_FOLDER = os.path.join(os.path.dirname(__file__), "..", "html")
 
 # ----------------------------
 # Create the Flask app
@@ -57,4 +57,3 @@ def send_request():
 # ----------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-print("WEB_FOLDER is:", WEB_FOLDER)
